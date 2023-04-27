cfcd95392d8739b9f369cf0879e8b974c7877cd3
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-04-27 15:40:32 -0500

    Create cloud-drive.ts

---
+++ cloud-drive.ts
@@ -0,0 +1,118 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as discord from 'discord.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Create a Discord client
+const client = new discord.Client();
+
+// When the client is ready, start the cloud drive
+client.on('ready', () => {
+  // Create a new cloud drive
+  const drive = new CloudDrive();
+
+  // Register the cloud drive with Discord
+  client.registerCommand('clouddrive', async (message, args) => {
+    // Check if the user has permission to use the cloud drive
+    if (!message.author.permissions.has('ADMINISTRATOR')) {
+      return message.reply('You do not have permission to use the cloud drive.');
+    }
+
+    // Get the command arguments
+    const command = args[0];
+    const path = args[1];
+
+    // Check the command
+    if (command === 'ls') {
+      // List the files in the current directory
+      const files = await drive.listFiles(path);
+      message.reply(files.map(file => file.name).join('\n'));
+    } else if (command === 'cd') {
+      // Change the current directory
+      await drive.changeDirectory(path);
+      message.reply(`Changed directory to ${path}`);
+    } else if (command === 'upload') {
+      // Upload a file
+      const file = await message.attachments.first();
+      await drive.uploadFile(file.filename, file.content, 500 * 1024 * 1024);
+      message.reply(`Uploaded ${file.filename}`);
+    } else if (command === 'download') {
+      // Download a file
+      const file = await drive.listFiles(path).find(file => file.name === args[2]);
+      const content = await drive.downloadFile(file.name);
+      message.channel.send(new discord.MessageAttachment(content, file.name));
+    } else {
+      // Unknown command
+      message.reply('Unknown command.');
+    }
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Cloud drive is running on port 
+3000.');
+});
+
+// Cloud drive class
+class CloudDrive {
+  constructor() {
+    this.files = {};
+  }
+
+  listFiles(path) {
+    // Check if the path is valid
+    if (!path) {
+      path = '/';
+    }
+
+    // Get the files in the specified directory
+    const files = Object.values(this.files);
+    if (path !== '/') {
+      files = files.filter(file => file.path.startsWith(path));
+    }
+
+    return files;
+  }
+
+  changeDirectory(path) {
+    // Check if the path is valid
+    if (!path) {
+      path = '/';
+    }
+
+    // Change the current directory
+    this.currentDirectory = path;
+  }
+
+  uploadFile(filename, content, chunkSize) {
+    // Check if the filename is valid
+    if (!filename) {
+      return;
+    }
+
+    // Upload the file in chunks
+    const chunks = content.chunks(chunkSize);
+    for (const chunk of chunks) {
+      this.files[filename] = {
+        name: filename,
+        path: this.currentDirectory,
+        content: chunk,
+      };
+    }
+  }
+
+  downloadFile(filename) {
+    // Check if the filename is valid
+    if (!filename) {
+      return;
+    }
+
+    // Download the file
+    const content = this.files[filename].content;
+    return content;
+  }
+}
